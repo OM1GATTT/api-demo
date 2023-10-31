ae8ba30d18a582dc9d838047478212c12e9255ad
OM1GA <845085139@qq.com>
2023-10-31 12:12:35 +0800

    CI/CD 7.2

--- src/main/java/top/om1ga/apidemo/ApiDemoApplication.java
+++ src/main/java/top/om1ga/apidemo/ApiDemoApplication.java
@@ -3,14 +3,20 @@ package top.om1ga.apidemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * @author  om1ga
+ */
 @RestController
 @SpringBootApplication
 public class ApiDemoApplication {
 
+    String oldIot=null;
     public static void main(String[] args) {
         SpringApplication.run(ApiDemoApplication.class, args);
     }
@@ -18,6 +24,16 @@ public class ApiDemoApplication {
     @GetMapping("/list")
     public List<String> getList(){
         return List.of("吃饭","睡觉","写代码");
+    }
+
+    @PostMapping("/iot")
+    public String postIot(@RequestBody String iot){
+        if (!iot.equals(oldIot)){
+            String temp = oldIot;
+            oldIot = iot;
+            return temp;
+        }
+        return oldIot;
     }
 
 }
